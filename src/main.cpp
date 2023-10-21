ed3946a4cc54aa0e29b0d6140ed3e01e3e21b693
Vitaliy Burkut <vitaliy.burkut@nexign.com>
2023-10-21 22:55:44 +0500

    after tests on a table

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@
 #define SECOND_MOTOR_STEPS 200                    // steps per Revolution
 #define SECOND_MOTOR_MICROSTEPS 16                // microsteps per step
 
-#define MIN_ELEVATION_DEGREE -20 // gear limit. look at construction
+#define MIN_ELEVATION_DEGREE -18 // gear limit. look at construction
 
 SpeedyStepper stepper1;
 SpeedyStepper stepper2;
@@ -47,6 +47,12 @@ volatile bool invert_direction = false;
 volatile bool IsMotorMotionProcessing = false;
 
 bool isHomeSetted = false;
+
+AltAzimuthRange altAzimuthRange;
+GpsData gp;
+LTMParser ltpParser;
+AzimuthInfo ai, ai_cur;
+int16_t fullRevolutions = 0;
 
 /* const u_int8_t MAV_SYS_ID = 200;
 const MAV_COMPONENT MAV_COMP = MAV_COMP_ID_SYSTEM_CONTROL; */
@@ -92,8 +98,8 @@ void secondMotorDriverSetup()
   stepper2.connectToPins(STEP_PIN2, DIR_PIN2);
   stepper2.setStepsPerRevolution((SECOND_MOTOR_STEPS * SECOND_MOTOR_MICROSTEPS * SECOND_MOTOR_GEAR_RATIO));
 
-  stepper2.setSpeedInRevolutionsPerSecond(2l);
-  stepper2.setAccelerationInRevolutionsPerSecondPerSecond(1l);
+  stepper2.setSpeedInRevolutionsPerSecond(0.8l);
+  stepper2.setAccelerationInRevolutionsPerSecondPerSecond(.4l);
 }
 
 void func_tim_4() // обработчик прерывания
@@ -111,11 +117,13 @@ void tim4_setup()
   tim4.resume();
 }
 
-// #define COMPASS_ENABLE
-//  #define GPS_ENABLE
+#define COMPASS_ENABLE
+#define GPS_ENABLE
+
+// #define BRUSH
 
 #ifdef COMPASS_ENABLE
-#define COMPASS_CALIBRATE_ROTATION_STEP 0.01
+#define COMPASS_CALIBRATE_ROTATION_STEP 0.005
 #define COMPASS_READ_SMOOTHY_COUNT 5
 
 #define COMPASS_ADR 0x0D
@@ -135,19 +143,15 @@ void setupCompass()
   compass.setSmoothing(COMPASS_READ_SMOOTHY_COUNT, false);
 }
 
-int compassReadSmoothy()
+void compassReadSmoothy()
 {
-  int err;
   digitalWrite(EN_PIN, HIGH); // Disable motors
   delay(100);
   for (int i = 0; i < COMPASS_READ_SMOOTHY_COUNT; i++)
   {
-    err = compass.read();
-    if (err > 0)
-      break;
+    compass.read();
   }
   digitalWrite(EN_PIN, LOW); // Enable motors
-  return err;
 }
 
 void setNorth()
@@ -161,8 +165,8 @@ void setNorth()
   {
     stepper1.setupMoveInRevolutions(i);
     while (!stepper1.motionComplete())
-      delay(10);
-    comp_err = compassReadSmoothy();
+      delay(2);
+    compassReadSmoothy();
 
     // comp_err = compass.read();
 
@@ -183,8 +187,8 @@ void setNorth()
     SerialUSB.println("-------");
     SerialUSB.print("Step:");
     SerialUSB.println(i);
-    SerialUSB.print("Cmp_err:");
-    SerialUSB.println(comp_err);
+    // SerialUSB.print("Cmp_err:");
+    // SerialUSB.println(comp_err);
     SerialUSB.print("xMin:");
     SerialUSB.println(xMin);
     SerialUSB.print("xMax:");
@@ -261,26 +265,34 @@ void updateGpsDate()
   byte c;
 
   // read and parse gps date
-  while (!gps.location.isValid())
+  while (!gps.location.isUpdated() || !gps.altitude.isUpdated())
     if (Serial1.available() > 0)
     {
       c = Serial1.read();
       if (c >= 0)
       {
-#ifdef DEBUG_INFO
+#ifdef DEBUG_GPS_INFO
         SerialUSB.write(c);
 #endif
         gps.encode(c);
       }
     }
 }
-
+#define GPS_AVG_COUNT 20
+#define max( a, b) (a > b ? a :b)
+#define min(a, b) (a < b ? a :b)
 void setLocation()
 {
-  SerialUSB.print("Start GPS reading...");
-  Serial1.begin(2400);
+  SerialUSB.println("Start GPS reading...");
+  Serial1.begin(9600);
 
-  double avgLat, avgLon, avgAlt;
+  double avgLat, avgLon, avgAlt, latMax, latMin, lonMin, lonMax, altMin, altMAx;
+  avgLat = 0;
+  avgLon = 0;
+  avgAlt = 0;
+  double latlist[GPS_AVG_COUNT];
+  double lonlist[GPS_AVG_COUNT];
+  double altlist[GPS_AVG_COUNT];
 
   for (uint16_t i = 0; i < MAX_WAIT_GPS_SEC * 2; i++)
   {
@@ -289,153 +301,203 @@ void setLocation()
 
 #ifdef DEBUG_INFO
     SerialUSB.print("hhop:");
-    SerialUSB.println(gps.hdop.hdop());
+    SerialUSB.print(gps.hdop.hdop());
+    SerialUSB.print(" sats:");
+    SerialUSB.println(gps.satellites.value());
 #endif
     if (gps.satellites.value() > 5 and gps.hdop.hdop() <= 10)
-      exit;
+      break;
     delay(500);
   }
-
-  for (uint8_t i = 0; i < 10; i++)
+  uint8_t i = 0;
+  do
   {
 
     updateGpsDate();
-    avgLat += gps.location.lat() / 10.0;
-    avgLon += gps.location.lng() / 10.0;
-    avgAlt += gps.altitude.value() / 10.0;
+    if (gps.location.isUpdated() && gps.altitude.isUpdated())
+    { 
+      latlist[i]= gps.location.lat();
+      lonlist[i] = gps.location.lng();
+      altlist[i] = gps.altitude.meters();
+
+      if (i ==0) { 
+        latMin = latMax = latlist[i];
+        lonMin = lonMax = lonlist[i];
+        altMin = altMAx = altlist[i];
+      
+      } else {
+        latMax = max (latMax, latlist[i]);
+        latMin = min (latMax, latlist[i]);
+        lonMax = max (lonMax, lonlist[i]);
+        lonMin = min (lonMin, lonlist[i]);
+        altMin = min (altMin, altlist[i]);
+        altMAx = max (altMAx, altlist[i]);
+      }
+
+
+      ++i;
+    }
+  }   
+    while (i < GPS_AVG_COUNT);
+  
+
+   for (i = 0; i < GPS_AVG_COUNT; i++) {
+
+    if (latMin != latlist[i] && latMax != latlist[i]) avgLat += latlist[i];
+    if (lonMax != lonlist[i] && lonMin != lonlist[i]) avgLon += lonlist[i];
+     if (altMAx != altlist[i] && altMin != altlist[i]) avgAlt += altlist[i];
+   }
+    avgLat /= GPS_AVG_COUNT - 2;
+    avgLon /= GPS_AVG_COUNT - 2;
+    avgAlt /= GPS_AVG_COUNT - 2;
+
+    altAzimuthRange.setObserverLocation({avgLat, avgLon, avgAlt});
+
+#ifdef DEBUG_INFO
+    SerialUSB.println("set home at:");
+    SerialUSB.print("Lat:");
+    SerialUSB.println(avgLat, 6);
+
+    SerialUSB.print("Lon:");
+    SerialUSB.println(avgLon, 6);
+
+    SerialUSB.print(" Alt:");
+    SerialUSB.print(avgAlt, 3);
+#endif
+
+    Serial1.end();
+    isHomeSetted = true;
   }
 
-  altAzimuthRange.setObserverLocation({avgLat, avgLon, avgAlt});
-
-  Serial1.end();
-  isHomeSetted = true;
-}
-
 #endif
 
-void setup()
-{
-  // delay(10000);
-  firstMotorDriverSetup();
-  secondMotorDriverSetup();
+  void setup()
+  {
+    // delay(10000);
+    firstMotorDriverSetup();
+    secondMotorDriverSetup();
 
-  pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  // tim4_setup();
+    // tim4_setup();
 
-  SerialUSB.begin();
+    SerialUSB.begin();
 
 #ifdef COMPASS_ENABLE
-  setupCompass();
-  setNorth();
+    setupCompass();
+    setNorth();
 #endif
 #ifdef GPS_ENABLE
-  setLocation();
+    setLocation();
 #endif
-}
+#ifdef BRUSH
+    stepper1.setupMoveInRevolutions(10);
+#endif
+  }
 
-/* mavlink_status_t status;
-mavlink_message_t msg;
-uint8_t chan = MAVLINK_COMM_0;
-mavlink_global_position_int_t gp;
-mavlink_command_int_t mci;
-mavlink_param_request_list_t prl; */
+  /* mavlink_status_t status;
+  mavlink_message_t msg;
+  uint8_t chan = MAVLINK_COMM_0;
+  mavlink_global_position_int_t gp;
+  mavlink_command_int_t mci;
+  mavlink_param_request_list_t prl; */
 
-GpsData gp;
-
-LTMParser ltpParser;
-
-AzimuthInfo ai, ai_cur;
-AltAzimuthRange altAzimuthRange;
-
-void loop()
-{
-
-  if (SerialUSB.available())
+  void loop()
   {
-    uint8_t byte = SerialUSB.read();
-    if (ltpParser.parseChar(byte) && ltpParser.isGpsDataUpdated())
+#ifndef BRUSH
+    if (SerialUSB.available())
     {
+      uint8_t byte = SerialUSB.read();
+      if (ltpParser.parseChar(byte) && ltpParser.isGpsDataUpdated())
+      {
 #ifdef DEBUG_INFO
-      SerialUSB.println("received LTP frame");
+        SerialUSB.println("received LTP frame");
 #endif
 
-      GPSFrameData_s telemetry = ltpParser.getGPSData();
-      if (isHomeSetted)
-      {
-        ai_cur = ai;
-        ai = altAzimuthRange.calculate({static_cast<FPD_TYPE>(telemetry.latitude / 1E7),
-                                        static_cast<FPD_TYPE>(telemetry.longitude / 1E7),
-                                        static_cast<FPD_TYPE>(telemetry.altitude / 1E2)});
-
-        if (ai.az - 180 > ai_cur.az)
+        GPSFrameData_s telemetry = ltpParser.getGPSData();
+        if (isHomeSetted)
         {
-          ai.az -= 360; // short way through zero
-        }
+          ai_cur = ai;
+          ai = altAzimuthRange.calculate({static_cast<FPD_TYPE>(telemetry.latitude / 1E7),
+                                          static_cast<FPD_TYPE>(telemetry.longitude / 1E7),
+                                          static_cast<FPD_TYPE>(telemetry.altitude / 1E2)});
 
-        else if (ai_cur.az - 180 > ai.az)
-        {
-          ai.az += 360; // short way through zero
-        }
+          ai.az+= 360 * fullRevolutions;                             
 
-        if (ai.ele < MIN_ELEVATION_DEGREE)
-          ai.ele = MIN_ELEVATION_DEGREE;
+          if (ai.az - 180 > ai_cur.az)
+          {
+            ai.az -= 360; // short way through zero
+            fullRevolutions--;
+          }
 
-        // if motors motion still incomplete skip new data
-        if (stepper1.motionComplete() && stepper2.motionComplete())
-        {
-          stepper1.setupMoveInRevolutions(ai.az / 360);
-          stepper2.setupMoveInRevolutions(ai.ele / 360);
+          else if (ai_cur.az - 180 > ai.az)
+          {
+            ai.az += 360; // short way through zero
+            fullRevolutions++;
+          }
+
+          if (ai.ele < MIN_ELEVATION_DEGREE)
+            ai.ele = MIN_ELEVATION_DEGREE;
+
+          // if motors motion still incomplete skip new data
+          if (stepper1.motionComplete() && stepper2.motionComplete())
+          {
+            stepper1.setupMoveInRevolutions(ai.az / 360);
+            stepper2.setupMoveInRevolutions(ai.ele / 360);
+          }
+          else
+          {
+#ifdef DEBUG_INFO
+            SerialUSB.println("motion incomplite. skeep moving");
+#endif
+          }
+
+#ifdef DEBUG_INFO
+          SerialUSB.println("Look at position:");
+          SerialUSB.print("lat:");
+          SerialUSB.println(static_cast<FPD_TYPE>(telemetry.latitude / 1E7), 8);
+          SerialUSB.print("lon:");
+          SerialUSB.println(static_cast<FPD_TYPE>(telemetry.longitude / 1E7), 8);
+          SerialUSB.print("alt:");
+          SerialUSB.println(static_cast<FPD_TYPE>(telemetry.altitude / 1E2), 8);
+          SerialUSB.println("-----");
+          SerialUSB.print("az:");
+          SerialUSB.println(ai.az, 8);
+          SerialUSB.print("ele:");
+          SerialUSB.println(ai.ele, 8);
+          SerialUSB.print("dist:");
+          SerialUSB.println(ai.dist, 8);
+
+#endif
         }
         else
         {
+          altAzimuthRange.setObserverLocation({static_cast<FPD_TYPE>(telemetry.latitude / 1E7),
+                                               static_cast<FPD_TYPE>(telemetry.longitude / 1E7),
+                                               static_cast<FPD_TYPE>(telemetry.altitude / 1E2)});
 #ifdef DEBUG_INFO
-          SerialUSB.println("motion incomplite. skeep moving");
+          SerialUSB.println("Set home position:");
+          SerialUSB.print("lat:");
+          SerialUSB.println(telemetry.latitude / 1E7);
+          SerialUSB.print("lon:");
+          SerialUSB.println(telemetry.longitude / 1E7);
+          SerialUSB.print("alt:");
+          SerialUSB.println(telemetry.altitude / 1E2);
 #endif
+
+          isHomeSetted = true;
         }
-
-#ifdef DEBUG_INFO
-        SerialUSB.println("Look at position:");
-        SerialUSB.print("lat:");
-        SerialUSB.println(static_cast<FPD_TYPE>(telemetry.latitude / 1E7), 8);
-        SerialUSB.print("lon:");
-        SerialUSB.println(static_cast<FPD_TYPE>(telemetry.longitude / 1E7), 8);
-        SerialUSB.print("alt:");
-        SerialUSB.println(static_cast<FPD_TYPE>(telemetry.altitude / 1E2), 8);
-        SerialUSB.println("-----");
-        SerialUSB.print("az:");
-        SerialUSB.println(ai.az, 8);
-        SerialUSB.print("ele:");
-        SerialUSB.println(ai.ele, 8);
-        SerialUSB.print("dist:");
-        SerialUSB.println(ai.dist, 8);
-
-#endif
-      }
-      else
-      {
-        altAzimuthRange.setObserverLocation({static_cast<FPD_TYPE>(telemetry.latitude / 1E7),
-                                             static_cast<FPD_TYPE>(telemetry.longitude / 1E7),
-                                             static_cast<FPD_TYPE>(telemetry.altitude / 1E2)});
-#ifdef DEBUG_INFO
-        SerialUSB.println("Set home position:");
-        SerialUSB.print("lat:");
-        SerialUSB.println(telemetry.latitude / 1E7);
-        SerialUSB.print("lon:");
-        SerialUSB.println(telemetry.longitude / 1E7);
-        SerialUSB.print("alt:");
-        SerialUSB.println(telemetry.altitude / 1E2);
-#endif
-
-        isHomeSetted = true;
       }
     }
+#else
+  if (stepper1.motionComplete())
+    stepper1.setupMoveInRevolutions(-stepper1.getCurrentPositionInRevolutions());
+#endif
+
+    motorMotionProcessing();
   }
 
-  motorMotionProcessing();
-}
-
-void yield(void)
-{
-  motorMotionProcessing();
-}
+  void yield(void)
+  {
+    motorMotionProcessing();
+  }
